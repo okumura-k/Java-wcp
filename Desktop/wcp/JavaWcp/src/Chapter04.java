91fc0cdda61b9f8fe14870573bf21216f5ccaa8f
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-04-18 02:29:57 +0900

    変数と定数と解説

---
+++ Desktop/wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		// 「String」型の変数「str」の宣言と初期化して出力
+		String str = "okumura";
+		System.out.println(str);
+
+		// 変数「str」に再代入して出力
+		str = "パンが無ければケーキを食べればいいじゃない";
+		System.out.println(str);
+
+		// 「int」型で宣言したクラス定数を出力
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
