dd655d43b3275c8e8be1fe0b93a4632bbadc6e8d
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-04-18 16:36:38 +0900

    列の連結

--- Desktop/wcp/JavaWcp/src/Chapter06.java
+++ Desktop/wcp/JavaWcp/src/Chapter06.java
@@ -1,22 +1,15 @@
 
 public class Chapter06 {
 	public static void main(String[] args) {
-		// 複数の式を条件とする場合に使用
+		// 文字列を連結する場合の演算子は算術演算子の足し算と同じ「+」
+		// a + b = a と b の文字列を連結する
 
-		// a && b = a と b の両方が true の場合に true
-		// a || b = a または b が true の場合に true
-		// !a = a が false の場合に true
+		String a = "100";
+		String b = "200";
+		String c;
 
-		boolean a = true;
-		boolean b = false;
-		boolean c;
-
-		c = a && true; // c = true
-		c = a && false; // c = false
-		c = b || true;
-		c = b || false;
-		c = !a;
-		c = !b;
+		c = a + b; // c = 100200
+		c = "あ" + "い"; // c = あい
 
 		System.out.println(c);
 	}
