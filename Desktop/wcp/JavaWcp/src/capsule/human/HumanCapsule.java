0fc0e8a48b9bb896b90c073f6236e472c5ececff
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-04-23 19:50:49 +0900

    カプセル化 getter setter

---
+++ Desktop/wcp/JavaWcp/src/capsule/human/HumanCapsule.java
@@ -0,0 +1,34 @@
+package capsule.human;
+
+public class HumanCapsule {
+	private String name;
+	private int age;
+
+	public HumanCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	// ageは外部から変更禁止なので、setterメソッドは作成しない
+	public int getAge() {
+		return this.age;
+	}
+}
+
+/*getterメソッド
+ * public 戻り値の型(フィールドの型)  getフィールド名() {
+    return this.フィールド名
+ }
+
+ // setterメソッド
+ public void setフィールド名(フィールドの型の引数) {
+    this.フィールド名 = フィールドの型の引数;
+ }*/
