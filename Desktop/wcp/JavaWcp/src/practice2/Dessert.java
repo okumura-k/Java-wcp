bcfc381ca3cd9f0cfa244575cc6d6f3d4c705d61
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-04-27 20:33:01 +0900

    列挙型　使い方

---
+++ Desktop/wcp/JavaWcp/src/practice2/Dessert.java
@@ -0,0 +1,8 @@
+package practice2;
+
+public enum Dessert {
+	APPLE, ICE_CREAM, CAKE,	
+	System.out.println(Dessert.APPLE);
+	System.out.println(Dessert.ICE_CREAM);
+	System.out.println(Dessert.CAKE);
+}
