6edcf5ebd6345653efd9b97878dde7e7d21dda31
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-04-27 20:16:56 +0900

    内部クラス　確認問題ス

--- Desktop/wcp/JavaWcp/src/practice2/Chapter01.java
+++ Desktop/wcp/JavaWcp/src/practice2/Chapter01.java
@@ -1,37 +1,10 @@
 package practice2;
 
-/*
-内部クラスは、外部クラスのフィールドを直接参照することが可
-
-public class Outer {
-
-    private String greeting = "こんにちは";
-
-    public class Inner {
-
-        public String hello() {
-            return greeting;
-        }
-    }
+public class Chapter01{
+	
+	public class HumanLeg{
+		public void kick() {
+			System.out.println("蹴る");
+		}
+	}
 }
-
-※内部クラスInnerがstaticな場合は、参照できすにコンパイルエラー
-
-参照されるフィールドもstaticな場合は、staticな内部クラスからも参照可
-
-public class Outer {
-
-  // staticなフィールドgreeting
-    private static String greeting = "こんにちは";
-
-  // staticな内部クラスInner
-    public static class Inner {
-
-        public String hello() {
-      // 外部クラスOuterのフィールドgreetingはstaticなので、参照可能
-            return greeting;
-        }
-    }
-
-}
- */
