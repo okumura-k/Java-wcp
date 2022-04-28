e43b8f316456613e4b733c1700afbecfb60686ef
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-04-28 23:41:53 +0900

    列挙型　活用

--- Desktop/wcp/JavaWcp/src/practice2/Dessert.java
+++ Desktop/wcp/JavaWcp/src/practice2/Dessert.java
@@ -1,8 +1,19 @@
 package practice2;
 
 public enum Dessert {
-	APPLE, ICE_CREAM, CAKE,	
-	System.out.println(Dessert.APPLE);
-	System.out.println(Dessert.ICE_CREAM);
-	System.out.println(Dessert.CAKE);
+	APPLE(100), ICE_CREAM(200), CAKE(300),;
+
+	private int price;
+
+	private Dessert(int price) {
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
+	System.out.println(Dessert.APPLE.getPrice());
+	System.out.println(Dessert.ICE_CREAM.getPrice());
+	System.out.println(Dessert.CAKE.getPrice());
 }
