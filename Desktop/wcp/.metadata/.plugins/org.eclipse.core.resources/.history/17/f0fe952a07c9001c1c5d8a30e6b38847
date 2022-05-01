da9bcff8df29ed77ef8df36c679dd96192433985
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-05-01 13:35:07 +0900

    定数　復習

---
+++ Desktop/wcp/.metadata/.plugins/org.eclipse.core.resources/.history/17/f0fe952a07c9001c1c5d8a30e6b38847
@@ -0,0 +1,13 @@
+
+public class Variable02 {
+	public static void main(String[] args) {
+		// 定数の宣言
+		// final 定数の型 定数名 ＝ 初期値；
+
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+
+		// 再代入しようとするとエラー
+		// MIN_NUMBER = 20;
+	}
+}
--- Desktop/wcp/JavaWcp/src/Variable02.java
+++ Desktop/wcp/JavaWcp/src/Variable02.java
@@ -1,13 +1,14 @@
 
 public class Variable02 {
 	public static void main(String[] args) {
-		// 定数の宣言
-		// final 定数の型 定数名 ＝ 初期値；
-
 		final int MIN_NUMBER = 10;
 		System.out.println(MIN_NUMBER);
-
-		// 再代入しようとするとエラー
-		// MIN_NUMBER = 20;
 	}
 }
+
+/*
+	・final修飾子基本文法
+		final 定数の型 定数名 = 初期値;
+*/
+
+//復習済み
