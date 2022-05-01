98f7092372d37fcf18ad69b3dd6edbd5b5f6a7f6
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-05-01 13:21:08 +0900

    エラー

--- Desktop/wcp/JavaWcp/src/practice2/Dessert.java
+++ Desktop/wcp/JavaWcp/src/practice2/Dessert.java
@@ -1,19 +1,7 @@
 package practice2;
 
 public enum Dessert {
-	APPLE(100), ICE_CREAM(200), CAKE(300),;
-
-	private int price;
-
-	private Dessert(int price) {
-		this.price = price;
-	}
-
-	public int getPrice() {
-		return price;
-	}
 	
-	System.out.println(Dessert.APPLE.getPrice());
-	System.out.println(Dessert.ICE_CREAM.getPrice());
-	System.out.println(Dessert.CAKE.getPrice());
 }
+
+//エラーのためreset
