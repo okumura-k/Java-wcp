76df202d060ad4041d20c73bb74446331ae47a05
okumura-k <96273004+okumura-k@users.noreply.github.com>
2022-05-05 06:04:54 +0900

    繰り返し　while文　復習

--- Desktop/wcp/JavaWcp/src/While01.java
+++ Desktop/wcp/JavaWcp/src/While01.java
@@ -1,18 +1,16 @@
 
 public class While01 {
 	public static void main(String[] args) {
-		// while文とは、指定の条件が「true」の間、繰り返し処理を行なう制御文
 		int number = 1;
-		// numberが50未満の場合、ブロックの処理を行なう
 		while (number < 50) {
-			// ブロック：
-            // numberにnumber * 2 を代入して、numberを出力
 			number *= 2;
-			System.out.println("while01 = " + number);
+			System.out.println("While01 = " + number);
 		}
 	}
 }
 
-/*	while (条件式) {
-	  ブロック（繰り返し処理）
-}*/
+/*	・基本文法
+ * 		while (条件式) {
+ * 		 ブロック（繰り返し処理） 
+ * 		}
+ */
